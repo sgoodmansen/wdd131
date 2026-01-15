ab0798b73f44fed65313f90ba9acf72bbdc16794
sgoodmansen <goo25023@byui.edu>
2026-01-15 13:07:11 -0700

    mission theme

---
+++ mission/mission.js
@@ -0,0 +1,24 @@
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+let selectBody = document.querySelector('body');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {
+        // code for changes to colors and logo
+        document.body.style.backgroundColor = "black";
+        logo.src = "images/byui-logo-white.png";
+        selectBody.style.color = "white";
+        document.getElementById("content").style.border = "1px solid white";
+
+    } else {
+        // code for changes to colors and logo
+        document.body.style.backgroundColor = "white";
+        logo.src = "images/byui-logo-blue.webp";
+        selectBody.style.color = "black";
+        document.getElementById("content").style.border = "1px solid black";
+    }
+}           
+                    
