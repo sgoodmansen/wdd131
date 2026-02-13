47e3ad87d346e54a1ff998691c82ac96ab9b71f6
sgoodmansen <sgoodmansen@gmail.com>
2026-02-13 13:42:09 -0700

    Movies - Fix JS

--- ponder/movies.js
+++ ponder/movies.js
@@ -50,12 +50,12 @@ const movies = [
 
         let html = 
             `<h2>${movie.title}</h2>
-            <img src=${movie.imgSrc} alt=${movie.imgAlt}>
+            <img src="${movie.imgSrc}" alt="${movie.imgAlt}">
             <p><strong>Release Date:</strong> ${movie.date}</p>
             <p><strong>Recommended Age:</strong> ${movie.ages}</p>
             <p><strong>Genre:</strong> ${movie.genre}</p>
             <p><strong>Rating:</strong> <span aria-label="${movie.stars.length} out of 5 stars" role="img">${movie.stars}</span></p>
-            <p id='desc'>${movie.description}</p>`
+            <p class='desc'>${movie.description}</p>`
 
         article.innerHTML = html;
         movieList.appendChild(article);
